335ffd6ba9ffd7ff3ba4961dc6d17122b72fd248
anjalee <anjalee@users.noreply.github.com>
2020-02-06 14:37:35 +0530

    initial commit

---
+++ WikiSearch/APIRequestFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  APIRequestFetcher.swift
+//  WikiSearch
+//
+//  Created by Anjalee on 2/4/20.
+//  Copyright © 2020 Anjalee Abenayaka. All rights reserved.
+//
+
+import Foundation
---
+++ WikiSearch/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  WikiSearch
+//
+//  Created by Anjalee on 2/4/20.
+//  Copyright © 2020 Anjalee Abenayaka. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
